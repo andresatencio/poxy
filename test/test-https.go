f41af76281a664292fa962085f65c172c51dcfb7
Fruta <and.atencio@gmail.com>
2018-07-17 02:29:29 -0300

    add exampĺe https

--- test/test-https.go
+++ test/test-https.go
@@ -1,39 +1,3 @@
-# Poxy
-
-A tiny lib for reverse proxy. Principal purpose is learn GoLang.
-
-![Poxy](poxipol.png "Poxy")
-
-
-## Install
-
-```sh
-go get -u github.com/andresatencio/poxy
-```
-
-## Examples
-
-Let's start registering a couple of endpoint URL and internal app:
-
-```go
-func main() {
-	poxy := poxy.NewPoxy()
-	poxy.Add("test.fruta:3000", "http://localhost:5000")
-	
-	http.HandleFunc("/", poxy.Run())
-	er := http.ListenAndServe(":3000", nil)
-
-	if er != nil {
-		fmt.Println(er)
-		panic(er)
-	}
-
-}
-```
-
-HTTPS:
-
-```go
 package main
 
 import (
@@ -69,5 +33,3 @@ func main() {
 	go http.ListenAndServe(":80", certManager.HTTPHandler(nil))
 	server.ListenAndServeTLS("", "")
 }
-
-```
